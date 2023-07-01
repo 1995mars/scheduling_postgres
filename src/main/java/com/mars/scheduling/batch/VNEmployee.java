b369efa1b9317683a3ac38f7df02516cb4a2aa5e
Mars <51732119+1995mars@users.noreply.github.com>
2023-07-01 11:02:25 +0700

    run java file in schedule

---
+++ src/main/java/com/mars/scheduling/batch/VNEmployee.java
@@ -0,0 +1,9 @@
+package com.mars.scheduling.batch;
+
+public class VNEmployee {
+
+    private void birthdayBatch(){
+        System.out.println("Birthday Employee");
+    }
+
+}
--- src/main/java/com/mars/scheduling/job/ScheduleJob.java
+++ src/main/java/com/mars/scheduling/job/ScheduleJob.java
@@ -4,10 +4,23 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class ScheduleJob implements Job {
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         // Xử lý công việc
+        try {
+            Class<?> clazz = Class.forName("com.mars.scheduling.batch.VNEmployee");
+            Method method = clazz.getDeclaredMethod("birthdayBatch");
+            method.setAccessible(true);
+            Object obj = clazz.newInstance();
+            method.invoke(obj);
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException |
+                 NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
         System.out.println(context + "-" + System.currentTimeMillis());
     }
 
